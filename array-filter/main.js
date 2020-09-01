450eaebde402ddbcef40ab9a9e9afc6a5ea95e91
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-01 13:18:50 -0700

    Completed array-filter

---
+++ array-filter/main.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars, no-console */
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna'
+];
+
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+
+const overFive = numbers.filter(number => number > 5);
+
+const startWithE = names.filter(name => name.startsWith('E'));
+
+const haveD = names.filter(name => {
+  if (name.includes('D') || name.includes('d')) {
+    return name;
+  }
+});
+
+console.log(evenNumbers);
+console.log(overFive);
+console.log(startWithE);
+console.log(haveD);
