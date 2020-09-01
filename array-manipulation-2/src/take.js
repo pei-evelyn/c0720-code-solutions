4182e9c593c25fb06a385e5e728ded9800ef8d86
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-01 12:49:08 -0700

    Worked on array-manipulation-2

--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+const take = (array, count) => {
+  const newArr = [];
+  if (array.length === 0) {
+    return newArr;
+  }
+  for (let i = 0; i < count; i++) {
+    newArr.push(array[i]);
+  }
+  return newArr;
+};
