9183f4fa29817e6e1de467cd3d8d1ab2bf43bb78
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-02 15:40:55 -0700

    Worked on chunk some more

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,31 +1,19 @@
 /* eslint-disable no-unused-vars */
-// const chunk = (array, size) => {
-//   if (array[0] === '') {
-//     return array;
-//   }
-//   const newArr = [];
-//   const ogLength = array.length;
-//   for (let i = 0; i < ogLength; i = i + size) {
-//     const subArr = array.slice(i, size);
-//     newArr.push(subArr);
-//   }
-//   return newArr;
-// };
-
-// const chunk = (array, size) => {
-//   const numOfArr = Math.ceil(array.length / size);
-//   const containerArr = [];
-//   for (let i = 0; i < numOfArr; i++) {
-//     const newArr = [];
-//     containerArr.push(newArr);
-//   }
-
-//   return containerArr;
-// };
 
 const chunk = (array, size) => {
   const containerArr = [];
-  for (let i = 0; i < array.length; i += size) {
-    const newArr = array.slice(i);
+  const arrLength = array.length;
+  for (let i = 0; i < arrLength; i = i + size) {
+    if (size === i) {
+      containerArr.push(array.slice(i, size + i));
+    } else {
+      containerArr.push(array.slice(i, size));
+    }
   }
+  return containerArr;
 };
+
+// newArr = [undefined, null, 0]
+// i = 0 + 3
+// .i = 3
+// newArr = []
