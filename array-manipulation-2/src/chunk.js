63bdf62affda9407b5522646f7de2f182bf94e12
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-01 23:14:57 -0700

    Worked on chunk some more

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,9 +1,20 @@
 /* eslint-disable no-unused-vars */
 const chunk = (array, size) => {
-  if (array.length === 0) {
+  if (array[0] === '') {
     return array;
   }
-  const leftOverArr = [];
+  const newArr = [];
+  const ogLength = array.length;
+  for (let i = 0; i < ogLength; i = i + size) {
+    if (i )
+    const subArr = array.slice(i, size);
+    newArr.push(subArr);
+
+  }
+  return newArr;
 };
 
-// return one central array with indexes of smaller arrays
+// index = 0,
+// array.length = 4;
+// i = 2;
+// size = 2;
