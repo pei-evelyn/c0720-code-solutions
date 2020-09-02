5f84ba9384a8160b447ee1aeabc32512958c30cb
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-02 10:10:41 -0700

    Worked on chunk some more

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,20 +1,31 @@
 /* eslint-disable no-unused-vars */
+// const chunk = (array, size) => {
+//   if (array[0] === '') {
+//     return array;
+//   }
+//   const newArr = [];
+//   const ogLength = array.length;
+//   for (let i = 0; i < ogLength; i = i + size) {
+//     const subArr = array.slice(i, size);
+//     newArr.push(subArr);
+//   }
+//   return newArr;
+// };
+
+// const chunk = (array, size) => {
+//   const numOfArr = Math.ceil(array.length / size);
+//   const containerArr = [];
+//   for (let i = 0; i < numOfArr; i++) {
+//     const newArr = [];
+//     containerArr.push(newArr);
+//   }
+
+//   return containerArr;
+// };
+
 const chunk = (array, size) => {
-  if (array[0] === '') {
-    return array;
+  const containerArr = [];
+  for (let i = 0; i < array.length; i += size) {
+    const newArr = array.slice(i);
   }
-  const newArr = [];
-  const ogLength = array.length;
-  for (let i = 0; i < ogLength; i = i + size) {
-    if (i )
-    const subArr = array.slice(i, size);
-    newArr.push(subArr);
-
-  }
-  return newArr;
 };
-
-// index = 0,
-// array.length = 4;
-// i = 2;
-// size = 2;
