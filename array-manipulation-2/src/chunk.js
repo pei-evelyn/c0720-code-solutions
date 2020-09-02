c1ea67f8d126b78347b3e60f1fc3e73cd79d228a
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-01 17:07:19 -0700

    worked on chunk

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-unused-vars */
+const chunk = (array, size) => {
+  if (array.length === 0) {
+    return array;
+  }
+  const leftOverArr = [];
+};
+
+// return one central array with indexes of smaller arrays
