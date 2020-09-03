573bfb4db910b5a5690ef8bae36d1edb1bd7e9e7
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-03 12:15:33 -0700

    worked on omit

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 
 const omit = (source, keys) => {
-  for (const keyName in source) {
+  const sourceCopy = source;
+  for (const keyName in sourceCopy) {
     for (let i = 0; i < keys.length; i++) {
       if (keyName === keys[i]) {
-        delete source[keys[i]];
+        delete sourceCopy[keys[i]];
       }
     }
   }
-  return source;
+  return sourceCopy;
 };
