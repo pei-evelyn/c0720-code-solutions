6c156cdb562bbe616c45819a900c4a9cc8a622d0
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-03 16:47:14 -0700

    Worked on optional question

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -8,9 +8,15 @@ for (let index = 2; index < process.argv.length; index++) {
   paths.push(process.argv[index]);
 }
 
-for (let counter = 0; counter < paths.length; counter++) {
-  fs.readFile(paths[counter], 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-}
+const callbackReadFile = (err, data) => {
+  if (err) throw err;
+  console.log(data);
+};
+
+const readMe = () => {
+  for (let counter = 0; counter < paths.length; counter++) {
+    fs.readFile(paths[counter], 'utf8', callbackReadFile);
+  }
+};
+
+setTimeout(readMe, 1000);
