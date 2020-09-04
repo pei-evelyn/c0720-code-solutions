0cf7c1e54d3641dd82d20f8aceb60f4d975a7eec
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-03 22:24:40 -0700

    Finished equal, started working on flatten

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+
+const equal = (first, second) => {
+  let counter = 0;
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] === second[i]) {
+      counter++;
+    }
+  }
+  return counter === first.length;
+};
