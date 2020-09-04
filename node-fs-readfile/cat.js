87475446c6793d7958de09d5e4b67796ca26990d
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-04 16:05:46 -0700

    Third times the charm???

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -2,16 +2,20 @@
 
 const fs = require('fs');
 
+const numOfFiles = process.argv.length - 2; // 2
 let index = 2;
+let filesRead = 0;
 
-const id = setInterval(() => {
-  if (index < process.argv.length) {
-    fs.readFile(process.argv[index], 'utf8', (err, data) => {
-      if (err) throw err;
-      console.log(data);
-      index += 1;
-    });
-  } else if (index === process.argv.length) {
-    clearInterval(id);
-  }
-}, 1000);
+const readMe = () => {
+  fs.readFile(process.argv[index], 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    filesRead++; // 1 2
+    index++; // 3 4
+    if (filesRead < numOfFiles) {
+      readMe();
+    }
+  });
+};
+
+readMe();
