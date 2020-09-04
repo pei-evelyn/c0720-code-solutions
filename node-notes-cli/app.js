3999c612441fd012d093b5a92fa9b484338b799f
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-04 14:43:11 -0700

    Added functionality: User can add note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,9 +1,21 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+
 const fs = require('fs');
 const JSONdata = require('./data.json');
 
 const read = require('./read');
+const add = require('./create');
 
-if (process.argv[2] === 'read') {
-  read();
+const action = process.argv[2];
+
+switch (action) {
+  case 'read':
+    read();
+    break;
+  case 'create':
+    add(process.argv[3]);
+    break;
+  default:
+    console.log(`Sorry, ${action} is not a valid action.`);
 }
