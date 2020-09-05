50d9aa50b38fef5d969438561e9fe26a0c3ec704
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-05 12:02:26 -0700

    Finished all problems but titleCase

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,5 +1,19 @@
 /* eslint-disable no-unused-vars */
 
 const titleCase = title => {
-  const minorWords = ['and', 'or', 'nor', ''];
+  const word = title.toLowerCase().split(' ');
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].length > 3) {
+      if (word[i] === 'javascript') {
+        word[i] = 'JavaScript';
+      } else if (word[i] === 'javascript:') {
+        word[i] = 'JavaScript:';
+      } else {
+        word[i] = word[i].charAt(0).toUpperCase() + word[i].slice(1);
+      }
+    }
+  }
+  // const pattern = /Javascript/;
+  // word.find;
+  return word.join(' ');
 };
