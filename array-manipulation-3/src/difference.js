ce4d89f9eff6248f41d69495b35aaafdc0426b3f
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-05 10:25:35 -0700

    Completed array-manipulation-3

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,12 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 const difference = (first, second) => {
-  const combinedArr = first.concat(second);
-  const result = [];
-  for (let i = 0; i < combinedArr.length; i++) {
-    if (combinedArr.indexOf(combinedArr[i]) === -1) {
-      result.push(combinedArr[i]);
-    }
-  }
-  return result;
+  const diff1 = first.filter(element => !second.includes(element));
+  const diff2 = second.filter(element => !first.includes(element));
+  return diff1.concat(diff2);
 };
--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,5 +1,13 @@
 /* eslint-disable no-unused-vars */
 
-const union = (first, second) => {
-
+const intersection = (first, second) => {
+  const newArr = [];
+  for (let firstIndex = 0; firstIndex < first.length; firstIndex++) {
+    for (let secondIndex = 0; secondIndex < second.length; secondIndex++) {
+      if (first[firstIndex] === second[secondIndex]) {
+        newArr.push(first[firstIndex]);
+      }
+    }
+  }
+  return newArr;
 };
