755cacb749cc2ec4c84fa7b1f9bac9538ea8f93c
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-08 21:20:22 -0700

    Finished titleCase T_T

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,19 +1,41 @@
 /* eslint-disable no-unused-vars */
 
 const titleCase = title => {
-  const word = title.toLowerCase().split(' ');
-  for (let i = 0; i < word.length; i++) {
-    if (word[i].length > 3) {
-      if (word[i] === 'javascript') {
-        word[i] = 'JavaScript';
-      } else if (word[i] === 'javascript:') {
-        word[i] = 'JavaScript:';
-      } else {
-        word[i] = word[i].charAt(0).toUpperCase() + word[i].slice(1);
-      }
+  const minorWords = ['in', 'of', 'the', 'for'];
+  const words = title.toLowerCase().split(' ');
+  const result = [];
+  for (let i = 0; i < words.length; i++) {
+    switch (words[i]) {
+      case minorWords[0]:
+      case minorWords[1]:
+      case minorWords[2]:
+      case minorWords[3]:
+        result.push(words[i]);
+        break;
+      case 'javascript':
+        words[i] = 'JavaScript';
+        result.push(words[i]);
+        break;
+      case 'javascript:':
+        words[i] = 'JavaScript:';
+        result.push(words[i]);
+        break;
+      case 'api':
+        words[i] = 'API';
+        result.push(words[i]);
+        break;
+      case 'in-depth':
+        words[i] = 'In-Depth';
+        result.push(words[i]);
+        break;
+      default:
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+        result.push(words[i]);
+        break;
+    }
+    if (words[1] === 'the') {
+      words[1] = words[1].charAt(0).toUpperCase() + words[1].slice(1);
     }
   }
-  // const pattern = /Javascript/;
-  // word.find;
-  return word.join(' ');
+  return result.join(' ');
 };
