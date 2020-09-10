994161e53d11528a0c61dc5cf7fac08af688a472
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-10 15:55:53 -0700

    Completed feature 2: clients can get single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -14,7 +14,29 @@ for (const note in jsonFile.notes) {
 // handle get requests for API notes array
 
 app.get('/api/notes', (req, res) => {
-  res.send(notes);
+  res.json(notes);
+});
+
+// handle get requests for single note
+
+app.get('/api/notes/:id', (req, res) => {
+  const parsedId = parseInt(req.params.id);
+
+  if (parsedId < 0) {
+    res.status(400).send({
+      error: 'id must be a positive integer'
+    });
+  } else {
+    const indexNum = notes.findIndex(note => note.id === parsedId);
+    if (indexNum === -1) {
+      res.status(404).send({
+        error: `cannot find note with id ${parsedId}`
+      });
+    } else {
+      res.json(notes[indexNum]);
+    }
+  }
+
 });
 
 app.listen(3000, () => {
