526de069e8a10e11f313d3bc77ae7b32fa516b3f
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-10 18:09:09 -0700

    Worked on client can add post

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 
 const jsonFile = require('./data.json');
+const fs = require('fs');
 
 const notes = [];
+const nextId = jsonFile.nextId;
 
 for (const note in jsonFile.notes) {
   notes.push(jsonFile.notes[note]);
@@ -23,20 +25,48 @@ app.get('/api/notes/:id', (req, res) => {
   const parsedId = parseInt(req.params.id);
 
   if (parsedId < 0) {
-    res.status(400).send({
+    res.status(400).json({
       error: 'id must be a positive integer'
     });
   } else {
     const indexNum = notes.findIndex(note => note.id === parsedId);
     if (indexNum === -1) {
-      res.status(404).send({
+      res.status(404).json({
         error: `cannot find note with id ${parsedId}`
       });
     } else {
       res.json(notes[indexNum]);
     }
   }
+});
 
+// parsing from JSON
+
+app.use(express.json());
+
+// handle post req to notes
+
+app.post('/api/notes', (req, res) => {
+  if (req.path === '/api/notes') {
+    const note = req.body;
+    if (JSON.stringify(note) === '{}') {
+      res.status(400).json({
+        error: 'content is a required field'
+      });
+    } else {
+      note.id = nextId;
+      const jsonFileNotes = jsonFile.notes;
+      jsonFileNotes[nextId] = note;
+      jsonFile.nextId++;
+      const toJSONString = JSON.stringify(jsonFile, null, '\t');
+      fs.writeFile('./data.json', toJSONString, err => {
+        if (err) throw err;
+      });
+      res.status(201).json(note);
+    }
+  } else {
+    res.status(500).json({ error: 'An unexpected error occurred.' });
+  }
 });
 
 app.listen(3000, () => {
