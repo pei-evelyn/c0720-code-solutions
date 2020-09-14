0a49a8eafbf98b1870df1c08149b13e13f410142
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-13 19:46:42 -0700

    Worked on title case

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,66 +1,36 @@
 /* eslint-disable no-unused-vars */
 
-// const titleCase = title => {
-//   const minorWords = ['in', 'of', 'the', 'for'];
-//   const words = title.toLowerCase().split(' ');
-//   const result = [];
-//   for (let i = 0; i < words.length; i++) {
-//     switch (words[i]) {
-//       case minorWords[0]:
-//       case minorWords[1]:
-//       case minorWords[2]:
-//       case minorWords[3]:
-//         result.push(words[i]);
-//         break;
-//       case 'javascript':
-//         words[i] = 'JavaScript';
-//         result.push(words[i]);
-//         break;
-//       case 'javascript:':
-//         words[i] = 'JavaScript:';
-//         result.push(words[i]);
-//         break;
-//       case 'api':
-//         words[i] = 'API';
-//         result.push(words[i]);
-//         break;
-//       case 'in-depth':
-//         words[i] = 'In-Depth';
-//         result.push(words[i]);
-//         break;
-//       default:
-//         words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-//         result.push(words[i]);
-//         break;
-//     }
-//     if (words[1] === 'the') {
-//       words[1] = words[1].charAt(0).toUpperCase() + words[1].slice(1);
-//     }
-//   }
-//   return result.join(' ');
-// };
-
 const titleCase = string => {
-  // capitalize first word of string and substring :
-  // string[0] capitalized
-  // string[i - 2] === ':' string[i] capitalized
-  string[0].toUpperCase();
-  for (let i = 0; i < string.length; i++) {
-    if (i === 0) {
-      string[i] = string[i].toUpperCase();
-    } else if (string[i - 2] === ':') {
-      string[i].toUpperCase();
+  let newString = '';
+  newString += string[0].toUpperCase();
+  for (let i = 1; i < string.length; i++) {
+    if (string[i - 2] === ':') {
+      newString += string[i].toUpperCase();
     } else if (string[i - 1] === '-') {
-      string[i].toUpperCase();
+      newString += string[i].toUpperCase();
+    } else if (string[i - 1] === ' ') {
+      newString += string[i].toUpperCase();
+    } else {
+      newString += string[i];
     }
   }
 
-  return string;
-  // capitalize all major words and second part of hyphenated major words
-  // string[i-1] === " " string[i] capitalized
-  // string[i-1] === "-" string[i]capitalized
+  const arr = newString.split(' ');
 
-  // capitalize all words of four letters or more
-  // word.length >= 4
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length < 4) {
+      arr[i] = arr[i][0].toLowerCase() + arr[i].slice(1);
+    } else if (arr[i] === 'Javascript') {
+      arr[i] = 'JavaScript';
+    } else if (arr[i] === 'Javascipt:') {
+      arr[i] = 'JavaScript:';
+    } else if (arr[i] === 'Api') {
+      arr[i] = 'API';
+    }
+  }
+
+  newString = arr.join(' ');
+
+  return newString;
 
 };
