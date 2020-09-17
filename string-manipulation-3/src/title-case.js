29965cdc31f24b56484b760f751c7e5bf045087d
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-17 09:42:33 -0700

    Fixed title case

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,14 +1,31 @@
 /* eslint-disable no-unused-vars */
 
 const titleCase = string => {
+  const minorWords = [
+    'and',
+    'or',
+    'nor',
+    'but',
+    'a',
+    'an',
+    'the',
+    'as',
+    'at',
+    'by',
+    'for',
+    'in',
+    'of',
+    'on',
+    'per',
+    'to'
+  ];
+
   let newString = '';
   newString += string[0].toUpperCase();
   for (let i = 1; i < string.length; i++) {
     if (string[i - 2] === ':') {
       newString += string[i].toUpperCase();
     } else if (string[i - 1] === '-') {
-      newString += string[i].toUpperCase();
-    } else if (string[i - 1] === ' ') {
       newString += string[i].toUpperCase();
     } else {
       newString += string[i];
@@ -18,19 +35,21 @@ const titleCase = string => {
   const arr = newString.split(' ');
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i].length < 4) {
-      arr[i] = arr[i][0].toLowerCase() + arr[i].slice(1);
-    } else if (arr[i] === 'Javascript') {
+    if (arr[i] === 'javascript' || arr[i] === 'Javascript') {
       arr[i] = 'JavaScript';
-    } else if (arr[i] === 'Javascipt:') {
+    } else if (arr[i] === 'javascipt:' || arr[i] === 'Javascript:') {
       arr[i] = 'JavaScript:';
-    } else if (arr[i] === 'Api') {
+    } else if (arr[i] === 'api') {
       arr[i] = 'API';
+    } else if (arr[i].length > 4) {
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    }
+
+    if (!minorWords.includes(arr[i]) && arr[i].length < 4) {
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
     }
   }
 
   newString = arr.join(' ');
-
   return newString;
-
 };
