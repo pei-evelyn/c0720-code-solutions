98c89f8fedebf525bb3c1ceb96a131c66ff85999
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-19 09:49:04 -0700

    Completed react-validated-input-component

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -7,61 +7,61 @@ class ValidatedInput extends React.Component {
       value: '',
       letterCounter: 0
     };
-    this.iIcon = 'times';
-    this.iColor = 'red';
-    this.statusMsgClass = 'red';
-    this.statusMsgContent = 'A password is required.';
     this.pwPattern = /\w{8,}/;
     this.handleChange = this.handleChange.bind(this);
-    this.pwDoesntPass = this.pwDoesntPass.bind(this);
-    this.pwPasses = this.pwPasses.bind(this);
   }
 
   handleChange(event) {
-
-    if (this.pwPattern.test(this.state.value)) {
-      this.pwPasses();
-      this.setState({
-        value: event.target.value,
-        letterCounter: this.state.letterCounter + 1
-      });
-    } else {
-      this.pwDoesntPass();
-      this.setState({
-        value: event.target.value,
-        letterCounter: this.state.letterCounter + 1
-      });
-    }
-  }
-
-  pwDoesntPass() {
-    this.iIcon = 'times';
-    this.iColor = 'red';
-    this.statusMsgClass = 'red';
-    this.statusMsgContent = 'Your password is too short.';
-  }
-
-  pwPasses() {
-    this.iIcon = 'check';
-    this.iColor = 'green';
-    this.statusMsgClass = 'hidden';
+    this.setState({
+      value: event.target.value,
+      letterCounter: this.state.letterCounter + 1
+    });
   }
 
   render() {
-    return (
-      <>
-        <form>
-          <label>Password</label>
-          <input type="password" value={this.state.value} onChange={this.handleChange}/>
-          <span>
-            <i className={`fas fa-${this.iIcon} ${this.iColor}`}></i>
-          </span>
-        </form>
-        <div>
-          <p className={`status-msg ${this.statusMsgClass}`}>{this.statusMsgContent}</p>
-        </div>
-      </>
-    );
+    if (this.state.letterCounter === 0) {
+      return (
+        <>
+          <form>
+            <label>Password</label>
+            <input type="password" value={this.state.value} onChange={this.handleChange} />
+            <span>
+              <i className="fas fa-times red"></i>
+            </span>
+          </form>
+          <div>
+            <p className="status-msg red">A password is required.</p>
+          </div>
+        </>
+      );
+    } else if (this.state.letterCounter < 8) {
+      return (
+        <>
+          <form>
+            <label>Password</label>
+            <input type="password" value={this.state.value} onChange={this.handleChange} />
+            <span>
+              <i className="fas fa-times red"></i>
+            </span>
+          </form>
+          <div>
+            <p className="status-msg red">Your password is too short.</p>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <form>
+            <label>Password</label>
+            <input type="password" value={this.state.value} onChange={this.handleChange} />
+            <span>
+              <i className="fas fa-check green"></i>
+            </span>
+          </form>
+        </>
+      );
+    }
   }
 
 }
