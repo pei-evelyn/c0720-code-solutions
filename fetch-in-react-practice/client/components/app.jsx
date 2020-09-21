9a660dad790f2c8aa21f0147c79c0d791c468404
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-09-20 22:31:46 -0700

    Completed fetch-in-react-practice

--- fetch-in-react-practice/client/components/app.jsx
+++ fetch-in-react-practice/client/components/app.jsx
@@ -18,36 +18,55 @@ class App extends React.Component {
   }
 
   getAllTodos() {
-    /**
-     * Use fetch to send a GET request to `/api/todos`.
-     * Then 😉, once the response JSON is received and parsed,
-     * update state with the received todos.
-     */
+    fetch('/api/todos')
+      .then(res => res.json())
+      .then(todos => this.setState({
+        todos: todos
+      }))
+      .catch(err => console.error(err));
   }
 
   addTodo(newTodo) {
-    /**
-     * Use fetch to send a POST request to `/api/todos`.
-     * Then 😉, once the response JSON is received and parsed,
-     * add the created todo to the state array.
-     *
-     * TIP: Be sure to SERIALIZE the todo in the body with JSON.stringify()
-     * And specify the "Content-Type" header as "application/json"
-     */
+    fetch('/api/todos', {
+      method: 'POST',
+      body: JSON.stringify(newTodo),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(todo => this.setState(
+        state => {
+          state.todos.push(todo);
+          return { todos: state.todos };
+        }))
+      .catch(err => console.error(err));
   }
 
   toggleCompleted(todoId) {
-    /**
-     * Find the index of the matching todo in the state array.
-     * Find its "isCompleted" status.
-     * Make a new Object containing the opposite "isCompleted" status.
-     * Use fetch to send a PATCH request to `/api/todos/${todoId}`
-     * Then 😉, once the response JSON is received and parsed,
-     * replace the old todo in state.
-     *
-     * TIP: Be sure to SERIALIZE the updates in the body with JSON.stringify()
-     * And specify the "Content-Type" header as "application/json"
-     */
+
+    const todoIndex = this.state.todos.findIndex(todo => todo.id === todoId);
+    const task = this.state.todos[todoIndex].task;
+    const isCompleted = this.state.todos[todoIndex].isCompleted;
+    const newObj = {
+      id: todoId,
+      task: task,
+      isCompleted: !isCompleted
+    };
+    fetch(`/api/todos/${todoId}`, {
+      method: 'PATCH',
+      body: JSON.stringify(newObj),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(todo =>
+        this.setState(state => {
+          state.todos[todoIndex] = todo;
+          return { todos: state.todos };
+        }))
+      .catch(err => console.error(err));
   }
 
   render() {
