e58ffbe447c178b2a5f898413ee234e4d9b84d9d
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-01 19:25:55 -0700

    Added handle click method to accordian app and view in state

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,24 +4,114 @@ class Accordian extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      view: 'javascript'
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    // const element = event.target;
+    if (this.state.view === 'html' ||
+      this.state.view === 'css' ||
+      this.state.view === 'javascript') {
+      this.setState({
+        view: 'closed'
+      });
+    }
+
+    // console.log(element.props);
+    // if (this.state.view === 'closed') {
+    //   this.setState({
+    //     view:
+    //   })
+    // }
   }
 
   render() {
-    return (
-      <>
-        <div>
-          <h2 className="border-bottom-none">Hypertext Markup Language</h2>
-        </div>
-        <div>
-          <h2 className="border-bottom-none">Cascading Style Sheets</h2>
-        </div>
-        <div>
-          <h2>JavaScript</h2>
-        </div>
-      </>
-    );
+    if (this.state.view === 'closed') {
+      return (
+        <>
+          <div onClick={this.handleClick}>
+            <h2 className="border-bottom-none" value="html">Hypertext Markup Language</h2>
+          </div>
+          <div onClick={this.handleClick}>
+            <h2 className="border-bottom-none" value="css">Cascading Style Sheets</h2>
+          </div>
+          <div onClick={this.handleClick}>
+            <h2 value="javascript">JavaScript</h2>
+          </div>
+        </>
+      );
+    }
+    if (this.state.view === 'html') {
+      return (
+        <>
+          <div onClick={this.handleClick}>
+            <h2 className="border-bottom-none" value="html">Hypertext Markup Language</h2>
+          </div>
+          <div>
+            <p className="border-bottom-none">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non labore
+              velit veniam, eius saepe, quisquam distinctio deleniti optio maxime
+              suscipit eveniet libero aliquam et tempora at reprehenderit cum,
+              eligendi tenetur.
+            </p>
+          </div>
+          <div onClick={this.handleClick}>
+            <h2 className="border-bottom-none" value="css">Cascading Style Sheets</h2>
+          </div>
+          <div onClick={this.handleClick}>
+            <h2 value="javascript">JavaScript</h2>
+          </div>
+        </>
+      );
+    }
+    if (this.state.view === 'css') {
+      return (
+        <>
+          <div onClick={this.handleClick}>
+            <h2 className="border-bottom-none" value="html">Hypertext Markup Language</h2>
+          </div>
+          <div onClick={this.handleClick}>
+            <h2 className="border-bottom-none" value="css">Cascading Style Sheets</h2>
+          </div>
+          <div>
+            <p className="border-bottom-none">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non labore
+              velit veniam, eius saepe, quisquam distinctio deleniti optio maxime
+              suscipit eveniet libero aliquam et tempora at reprehenderit cum,
+              eligendi tenetur.
+            </p>
+          </div>
+          <div onClick={this.handleClick}>
+            <h2 value="javascript">JavaScript</h2>
+          </div>
+        </>
+      );
+    }
+    if (this.state.view === 'javascript') {
+      return (
+        <>
+          <div onClick={this.handleClick}>
+            <h2 className="border-bottom-none" value="html">Hypertext Markup Language</h2>
+          </div>
+          <div onClick={this.handleClick}>
+            <h2 className="border-bottom-none" value="css">Cascading Style Sheets</h2>
+          </div>
+          <div onClick={this.handleClick}>
+            <h2 className="border-bottom-none" value="javascript">JavaScript</h2>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non labore
+              velit veniam, eius saepe, quisquam distinctio deleniti optio maxime
+              suscipit eveniet libero aliquam et tempora at reprehenderit cum,
+              eligendi tenetur.
+            </p>
+          </div>
+        </>
+      );
+    }
   }
 }
 
