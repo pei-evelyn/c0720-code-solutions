df9af38ac3d467324456bb1557f4a53799087c8c
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-03 19:56:57 -0700

    Redid accordian to receive an array of objects via props

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,129 +1,74 @@
 import React from 'react';
 
-class Accordian extends React.Component {
+class Topic extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isClosed: true,
-      htmlDisplay: 'hidden',
-      cssDisplay: 'hidden',
-      jsDisplay: 'hidden'
+      status: 'hidden'
     };
-    this.handleHtmlClick = this.handleHtmlClick.bind(this);
-    this.handleCssClick = this.handleCssClick.bind(this);
-    this.handleJsClick = this.handleJsClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleHtmlClick() {
-    if (!this.state.isClosed) {
-      this.setState(state => {
-        return {
-          isClosed: !state.isClosed,
-          htmlDisplay: 'hidden',
-          cssDisplay: 'hidden',
-          jsDisplay: 'hidden'
-        };
-      });
-    }
-
-    if (this.state.isClosed) {
-      this.setState(state => {
-        return {
-          isClosed: !state.isClosed,
-          htmlDisplay: '',
-          cssDisplay: 'hidden',
-          jsDisplay: 'hidden'
-        };
-      });
-    }
-  }
-
-  handleCssClick() {
-    if (!this.state.isClosed) {
-      this.setState(state => {
-        return {
-          isClosed: !state.isClosed,
-          htmlDisplay: 'hidden',
-          cssDisplay: 'hidden',
-          jsDisplay: 'hidden'
-        };
-      });
-    }
-
-    if (this.state.isClosed) {
-      this.setState(state => {
-        return {
-          isClosed: !state.isClosed,
-          cssDisplay: '',
-          htmlDisplay: 'hidden',
-          jsDisplay: 'hidden'
-        };
-      });
-    }
-  }
-
-  handleJsClick() {
-    if (!this.state.isClosed) {
-      this.setState(state => {
-        return {
-          isClosed: !state.isClosed,
-          htmlDisplay: 'hidden',
-          cssDisplay: 'hidden',
-          jsDisplay: 'hidden'
-        };
-      });
-    }
-
-    if (this.state.isClosed) {
-      this.setState(state => {
-        return {
-          isClosed: !state.isClosed,
-          jsDisplay: '',
-          htmlDisplay: 'hidden',
-          cssDisplay: 'hidden'
-        };
-      });
-    }
+  handleClick() {
+    this.setState({
+      status: ''
+    });
   }
 
   render() {
     return (
       <>
-        <div onClick={this.handleHtmlClick}>
-          <h2 className="border-bottom-none" view="html">Hypertext Markup Language</h2>
+        <div onClick={this.handleClick}>
+          <h2>{this.props.topicName}</h2>
         </div>
-        <div className={this.state.htmlDisplay}>
-          <p className="border-bottom-none">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non labore
-              velit veniam, eius saepe, quisquam distinctio deleniti optio maxime
-              suscipit eveniet libero aliquam et tempora at reprehenderit cum,
-              eligendi tenetur.
-          </p>
-        </div>
-        <div onClick={this.handleCssClick}>
-          <h2 className="border-bottom-none" view="css">Cascading Style Sheets</h2>
-        </div>
-        <div className={this.state.cssDisplay}>
-          <p className="border-bottom-none">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non labore
-            velit veniam, eius saepe, quisquam distinctio deleniti optio maxime
-            suscipit eveniet libero aliquam et tempora at reprehenderit cum,
-            eligendi tenetur.
-          </p>
-        </div>
-        <div onClick={this.handleJsClick}>
-          <h2 view="javascript">JavaScript</h2>
-        </div>
-        <div className={this.state.jsDisplay}>
-          <p className="top-border-none">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non labore
-              velit veniam, eius saepe, quisquam distinctio deleniti optio maxime
-              suscipit eveniet libero aliquam et tempora at reprehenderit cum,
-              eligendi tenetur.
-          </p>
+        <div className={this.state.status}>
+          <p>{this.props.topicContent}</p>
         </div>
       </>
     );
+  }
+}
+
+class Accordian extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topics: []
+    };
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      topics: this.props.topicList
+    });
+  }
+
+  // handleClick(event) {
+  //   console.log(event);
+  // }
+
+  render() {
+    if (this.state.topics.length > 0) {
+      const topicContianers = this.state.topics.map(topic =>
+        <Topic
+          key={topic.id}
+          topicName={topic.name}
+          topicContent={topic.content}
+          handleClick={this.handleClick}
+          view={this.state.view}
+        />
+      );
+      // console.log(topicContianers);
+      return (
+        <>
+          {topicContianers}
+        </>
+      );
+    } else {
+      return null;
+    }
+
   }
 }
 
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,7 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordian from './accordion';
 
+const topics = [
+  {
+    name: 'Hypertext Markup Language',
+    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non labore
+              velit veniam, eius saepe, quisquam distinctio deleniti optio maxime
+              suscipit eveniet libero aliquam et tempora at reprehenderit cum,
+              eligendi tenetur.`,
+    id: 'html'
+  },
+  {
+    name: 'Cascading Style Sheets',
+    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non labore
+              velit veniam, eius saepe, quisquam distinctio deleniti optio maxime
+              suscipit eveniet libero aliquam et tempora at reprehenderit cum,
+              eligendi tenetur.`,
+    id: 'css'
+  },
+  {
+    name: 'JavaScript',
+    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non labore
+              velit veniam, eius saepe, quisquam distinctio deleniti optio maxime
+              suscipit eveniet libero aliquam et tempora at reprehenderit cum,
+              eligendi tenetur.`,
+    id: 'js'
+  }
+];
+
 ReactDOM.render(
-  <Accordian/>,
+  <Accordian topicList={topics}/>,
   document.getElementById('root')
 );
