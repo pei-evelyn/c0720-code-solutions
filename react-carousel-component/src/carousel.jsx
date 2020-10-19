4ba79d2e47be87634fcf56726e79c246c377d40a
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-18 21:53:39 -0700

    Added componentdidmount method to set state

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -4,38 +4,61 @@ class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pictureId: null
+      pictureId: null,
+      pictures: []
     };
+    // this.handleBackIconClick = this.handleBackIconClick.bind(this);
+    // this.handleNextIconClick = this.handleNextIconClick.bind(this);
   }
 
+  componentDidMount() {
+    this.setState({
+      pictures: this.props.images
+    });
+  }
+
+  setPictureId() {
+
+  }
+
+  // handleNextIconClick() {
+  //   console.log('back');
+  // }
+
+  // handleBackIconClick() {
+  //   console.log('next');
+  // }
+
   render() {
-    return (
-      <div className="carousel-container">
-        <div className="img-row">
-          <div className="col-icon left">
-            <i className="fas fa-3x fa-angle-left"></i>
+    if (!this.state.pictureId) {
+      return (
+        <div className="carousel-container">
+          <div className="img-row">
+            <div className="col-icon left">
+              <i className="fas fa-3x fa-angle-left" onClick={() => this.handleBackIconClick()}></i>
+            </div>
+            <div className="col-img">
+              <img
+                className="carousel-img"
+                src="https://images.saymedia-content.com/.image/t_share/MTc0NDU3MDA2NTUwNDI3Mjcw/neko-atsume-character-profile-sunny.png"
+                alt="" />
+            </div>
+            <div className="col-icon right">
+              <i className="fas fa-3x fa-angle-right" onClick={() => this.handleNextIconClick()}></i>
+            </div>
           </div>
-          <div className="col-img">
-            <img
-              className="carousel-img"
-              src="https://images.saymedia-content.com/.image/t_share/MTc0NDU3MDA2NTUwNDI3Mjcw/neko-atsume-character-profile-sunny.png"
-              alt="" />
-          </div>
-          <div className="col-icon right">
-            <i className="fas fa-3x fa-angle-right"></i>
+          <div className="row">
+            <div className="col circle-col">
+              <div className="circle-outline"></div>
+              <div className="circle-outline"></div>
+              <div className="circle-outline"></div>
+              <div className="circle-outline"></div>
+              <div className="circle-outline"></div>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col circle-col">
-            <div className="circle-outline"></div>
-            <div className="circle-outline"></div>
-            <div className="circle-outline"></div>
-            <div className="circle-outline"></div>
-            <div className="circle-outline"></div>
-          </div>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
