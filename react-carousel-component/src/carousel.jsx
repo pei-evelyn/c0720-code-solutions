a4da426431b054470c88c787a5981c16716d8c33
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-21 22:14:54 -0700

    Added handleClick to back and forth buttons

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -4,11 +4,12 @@ class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pictureId: null,
+      pictureId: 1,
       pictures: []
     };
-    // this.handleBackIconClick = this.handleBackIconClick.bind(this);
-    // this.handleNextIconClick = this.handleNextIconClick.bind(this);
+    this.handleBackIconClick = this.handleBackIconClick.bind(this);
+    this.handleNextIconClick = this.handleNextIconClick.bind(this);
+    this.getPictureUrl = this.getPictureUrl.bind(this);
   }
 
   componentDidMount() {
@@ -17,48 +18,63 @@ class Carousel extends React.Component {
     });
   }
 
-  setPictureId() {
-
+  getPictureUrl() {
+    let result;
+    for (let i = 0; i < this.state.pictures.length; i++) {
+      if (this.state.pictureId === this.state.pictures[i].id) {
+        result = this.state.pictures[i].imageUrl;
+      }
+    }
+    return result;
   }
 
-  // handleNextIconClick() {
-  //   console.log('back');
-  // }
+  handleNextIconClick() {
+    const length = this.state.pictures.length;
+    this.setState(state => {
+      if (state.pictureId === length) {
+        const id = 1;
+        return { pictureId: id };
+      }
+      return { pictureId: state.pictureId + 1 };
+    });
+  }
 
-  // handleBackIconClick() {
-  //   console.log('next');
-  // }
+  handleBackIconClick() {
+    const length = this.state.pictures.length;
+    this.setState(state => {
+      if (state.pictureId === 1) {
+        const id = length;
+        return { pictureId: id };
+      }
+      return { pictureId: state.pictureId - 1 };
+    });
+  }
 
   render() {
-    if (!this.state.pictureId) {
-      return (
-        <div className="carousel-container">
-          <div className="img-row">
-            <div className="col-icon left">
-              <i className="fas fa-3x fa-angle-left" onClick={() => this.handleBackIconClick()}></i>
-            </div>
-            <div className="col-img">
-              <img
-                className="carousel-img"
-                src="https://images.saymedia-content.com/.image/t_share/MTc0NDU3MDA2NTUwNDI3Mjcw/neko-atsume-character-profile-sunny.png"
-                alt="" />
-            </div>
-            <div className="col-icon right">
-              <i className="fas fa-3x fa-angle-right" onClick={() => this.handleNextIconClick()}></i>
-            </div>
+    return (
+      <div className="carousel-container">
+        <div className="img-row">
+          <div className="col-icon left">
+            <i className="fas fa-3x fa-angle-left" onClick={() => this.handleBackIconClick()}></i>
           </div>
-          <div className="row">
-            <div className="col circle-col">
-              <div className="circle-outline"></div>
-              <div className="circle-outline"></div>
-              <div className="circle-outline"></div>
-              <div className="circle-outline"></div>
-              <div className="circle-outline"></div>
-            </div>
+          <div className="col-img">
+            <img className="carousel-img" src={this.getPictureUrl()} />
+          </div>
+          <div className="col-icon right">
+            <i className="fas fa-3x fa-angle-right" onClick={() => this.handleNextIconClick()}></i>
           </div>
         </div>
-      );
-    }
+        <div className="row">
+          <div className="col circle-col">
+            <div className="circle-outline"></div>
+            <div className="circle-outline"></div>
+            <div className="circle-outline"></div>
+            <div className="circle-outline"></div>
+            <div className="circle-outline"></div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
